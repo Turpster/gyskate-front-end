a0faea4e0be3da1039d60ac6ded8652e90ec185d
turpster <turpster@turpster.net>
2020-01-01 23:44:22 +0000

    Added index page.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import "./index.css"
+import NavBarPage from "./pages/nav-bar/nav-bar-page";
+
+ReactDOM.render(
+    <NavBarPage/>,
+    document.getElementById('root'));
