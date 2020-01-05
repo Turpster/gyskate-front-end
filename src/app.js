a8f3c5ec7690da09b22126addf400178a8af7f16
turpster <turpster@turpster.net>
2020-01-05 20:19:26 +0000

    Renamed index.js, index.css to app.js, app.css

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import "./index.css"
+import "./app.css"
 import NavBarPage from "./pages/components/nav-bar/nav-bar-page";
 
 ReactDOM.render(
